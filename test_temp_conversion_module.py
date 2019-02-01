81db47cc4cb558d24292f05403eb1733f5efda20
Mackenzie Willborn <mackenziewillborn@Mackenzies-MacBook-Pro.local>
2019-02-01 13:42:20 -0500

    add files for travis ci testing

---
+++ test_temp_conversion_module.py
@@ -0,0 +1,17 @@
+def test_convert_c_to_f():
+    from tempconversion import convert_c_to_f
+    answer = convert_c_to_f(20.0)
+    expected = 68.0
+    assert answer == expected 
+def test_2():
+    answer = convert_c_to_f(-40.0)
+    answepected = (-40.0)
+    assert answer == expected 
+
+def test_fever_detection():
+    temp_list = [93.0, 98.0]
+    max_temp, is_fever = 
+    expected_max = 98.0
+    is_fever = True 
+
+    assert max_temp == expected max 
